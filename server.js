f64dba8ebcddb40f177360ed74bcb6fd5a56519e
sherwin <sherwin.web.ph@tripleonetech.net>
2020-07-12 02:46:53 +0800

    test4

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.use(express.json());
 
 app.use('/clients/data', clientsRouter);
 
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (res, req) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
+// this is a comment and and
+
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 
