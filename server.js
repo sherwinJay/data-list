d5c9c927ae76a1feb83804f10f986fc641b5dd89
sherwin <sherwin.web.ph@tripleonetech.net>
2020-07-12 15:56:13 +0800

    test8

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
     // static folder
     console.log("Current directory:", __dirname); 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve("client", "build", "index.html"));
+        res.sendFile(path.resolve("./", "client", "build", "index.html"));
     });
 }
 
