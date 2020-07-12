076504d4924575db6032bae2465453ff6a41aa2f
sherwin <sherwin.web.ph@tripleonetech.net>
2020-07-12 16:07:57 +0800

    test9

---
+++ build/precache-manifest.845f8f7f637e16f12f3f31152a0ce48a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef326bcc51afb46c77e93b4f6d95f80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdc77c2aeb840b033a35",
+    "url": "/static/css/main.57611d55.chunk.css"
+  },
+  {
+    "revision": "bb868f45384069227fc0",
+    "url": "/static/js/2.5dc190da.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/static/js/2.5dc190da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdc77c2aeb840b033a35",
+    "url": "/static/js/main.d4c0cd5e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
