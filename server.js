b7bbef5767bf8f12c6382eaabdf47c92a43ce1ee
sherwin <sherwin.web.ph@tripleonetech.net>
2020-07-13 14:19:45 +0800

    test18

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, 'client', 'build')));
     // static folder
     console.log("Current directory:", __dirname); 
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
     // app.use(express.static(path.join(__dirname, 'build')));
