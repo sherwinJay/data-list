60b651ff0ca243eb94120e8836d5ec146fe282d3
sherwin <sherwin.web.ph@tripleonetech.net>
2020-07-13 23:42:11 +0800

    test18

---
+++ build/precache-manifest.fd4e80354bb15bb7a369e1041941720a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ecf69b194ba68b0d4152dcb99d29309",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fd463d13f4f4c001471",
+    "url": "/static/css/main.57611d55.chunk.css"
+  },
+  {
+    "revision": "7429a85b3ab5d3816264",
+    "url": "/static/js/2.3ac11e1c.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/static/js/2.3ac11e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd463d13f4f4c001471",
+    "url": "/static/js/main.0bbea24b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7e09b211041ba494c9b167d571eb3d2.js"
+  "/precache-manifest.fd4e80354bb15bb7a369e1041941720a.js"
 );
 
 self.addEventListener('message', (event) => {
